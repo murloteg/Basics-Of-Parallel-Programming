ccc943767b91788a4dc4c7c133aa208ecb37f354
Kirill Bolotov <k.y.bolotov@gmail.com>
2023-02-14 20:58:37 +0700

    Fixed many problems like wrong answers [e.g. in case with huge start parameter N]

--- lab1-version-1/main.cpp
+++ lab1-version-1/main.cpp
@@ -2,9 +2,9 @@
 #include <cmath>
 #include <mpi.h>
 
-const int SIZE = 4;
-const double EPSILON = 0.00001;
-const double TAU = 0.01;
+const int ORIGIN_SIZE = 1024;
+const double EPSILON = 1e-7;
+const double TAU = 1e-5;
 
 double* GenerateVectorOfSolution(int size) {
     auto vector = new double[size];
@@ -14,40 +14,85 @@ double* GenerateVectorOfSolution(int size) {
     return vector;
 }
 
-double* GenerateVectorOfRightSide(int size, bool statusOfChangeOfSize, int originSize) {
+double* GenerateVectorOfRightSide(int size, bool statusOfChangeOfSize) {
     auto vector = new double[size];
-    for (int i = 0; i < originSize; ++i) {
-        vector[i] = size + 1;
+    for (int i = 0; i < ORIGIN_SIZE; ++i) {
+        vector[i] = ORIGIN_SIZE + 1;
     }
     if (statusOfChangeOfSize) {
-        for (int i = originSize; i < size; ++i) {
+        for (int i = ORIGIN_SIZE; i < size; ++i) {
             vector[i] = 0;
         }
     }
     return vector;
 }
 
-double* GeneratePartOfMatrix(int rank, int numberOfProcesses, int size) {
-    int matrixSize = 0;
-    if (size % numberOfProcesses != 0) {
-        // ... TODO: do it later.
+void PrintVector(double* vector, int size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << vector[i] << " ";
     }
-    else {
+    std::cout << std::endl;
+}
+
+void DebugPrint(double* matrix, int n, int m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            std::cout << matrix[i * m + j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int GetNumberOfExtraRows(int size) {
+    return (size - ORIGIN_SIZE);
+}
+
+int GetLastElementFromArray(int* array, int size) {
+    return array[size - 1];
+}
+
+double* GeneratePartOfMatrix(int rank, int numberOfProcesses, int size, bool statusOfChangeOfSize) {
+    int matrixSize;
+    if (statusOfChangeOfSize) {
         matrixSize = size * (size / numberOfProcesses);
     }
-    auto matrix = new double[matrixSize];
-    for (int i = 0; i < matrixSize; ++i) {
-        matrix[i] = 1;
+    else {
+        matrixSize = ORIGIN_SIZE * (ORIGIN_SIZE / numberOfProcesses);
     }
-    int rows = (size / numberOfProcesses);
-    int startPosition = rows * rank;
-    for (int i = 0; i < rows; ++i) {
+    int numberOfRows = (size / numberOfProcesses);
+    auto matrix = new double[matrixSize];
+    for (int i = 0; i < numberOfRows; ++i) {
+        for (int j = 0; j < ORIGIN_SIZE; ++j) {
+            matrix[i * size + j] = 1;
+        }
+    }
+    int startPosition = numberOfRows * rank;
+    for (int i = 0; i < numberOfRows; ++i) {
         if (startPosition > matrixSize) {
             break;
         }
         matrix[startPosition] = 2;
         startPosition = startPosition + size + 1;
     }
+    auto arrayWithRowNumbers = new int[numberOfRows];
+    for (int i = 0; i < numberOfRows; ++i) {
+        arrayWithRowNumbers[i] = i + rank * numberOfRows + 1;
+    }
+
+    int numberOfExtraRows = GetNumberOfExtraRows(size);
+    int extraRowPosition = size - numberOfExtraRows;
+    if (GetLastElementFromArray(arrayWithRowNumbers, numberOfRows) > extraRowPosition) {
+        for (int i = 0; i < numberOfRows; ++i) {
+            if (arrayWithRowNumbers[i] > extraRowPosition) {
+                for (int j = 0; j < size; ++j) {
+                    matrix[i * size + j] = 0;
+                }
+            }
+        }
+    }
+//    DebugPrint(matrix, numberOfRows, size);
+//    printf("\n");
+    delete[] arrayWithRowNumbers;
     return matrix;
 }
 
@@ -65,17 +110,8 @@ double GetSecondNormOfVector(const double* vector, int size) {
     return sqrt(totalSum);
 }
 
-void DebugPrint(double* matrix, int n, int m) {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            std::cout << matrix[i * m + j] << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
-double* MultiplyMatrixByVector(double* vector, int size, int rank, int numberOfProcesses) {
-    double* partOfMatrix = GeneratePartOfMatrix(rank, numberOfProcesses, size);
+double* MultiplyMatrixByVector(double* vector, int size, int rank, int numberOfProcesses, bool statusOfChangeOfSize) {
+    double* partOfMatrix = GeneratePartOfMatrix(rank, numberOfProcesses, size, statusOfChangeOfSize);
     int columns = size / numberOfProcesses;
     auto vectorOfResult = new double[columns];
     for (int i = 0; i < columns; ++i) {
@@ -116,26 +152,19 @@ void CopyVectorToVector(double* source, double* destination, int size) {
     }
 }
 
-void PrintVector(double* vector, int size) {
-    for (int i = 0; i < size; ++i) {
-        std::cout << vector[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
 void CleanUp(double* vectorOfSolution, double* vectorOfRightSide) {
     delete[] vectorOfSolution;
     delete[] vectorOfRightSide;
 }
 
-double* MethodOfSimpleIteration(double* vectorOfSolution, double* vectorOfRightSide, int rank, int size, int numberOfProcesses, bool statusOfChangeOfSize, int originSize) {
+double* MethodOfSimpleIteration(double* vectorOfSolution, double* vectorOfRightSide, int rank, int size, int numberOfProcesses, bool statusOfChangeOfSize) {
     auto totalResult = new double[size];
     auto tempVector = new double[size];
-    double lowerPartSecondNorm = GetSecondNormOfVector(vectorOfRightSide, SIZE);
-    double upperPartSecondNorm = GetSecondNormOfVector(MultiplyVectorByConst(vectorOfRightSide, SIZE, -1), SIZE);
-    int columns = SIZE / numberOfProcesses;
+    double lowerPartSecondNorm = GetSecondNormOfVector(vectorOfRightSide, size);
+    double upperPartSecondNorm = GetSecondNormOfVector(MultiplyVectorByConst(vectorOfRightSide, size, -1), size);
+    int columns = size / numberOfProcesses;
     while (!CheckStopCondition(upperPartSecondNorm, lowerPartSecondNorm)) {
-        double* partOfResultOfMultiplication = MultiplyMatrixByVector(vectorOfSolution, size, rank, numberOfProcesses);
+        double* partOfResultOfMultiplication = MultiplyMatrixByVector(vectorOfSolution, size, rank, numberOfProcesses, statusOfChangeOfSize);
         MPI_Allgather(partOfResultOfMultiplication, columns, MPI_DOUBLE, tempVector, columns, MPI_DOUBLE, MPI_COMM_WORLD);
 //        PrintVector(tempVector, size);
         double* resultOfSubtraction = SubtractionOfVectors(tempVector, vectorOfRightSide, size);
@@ -172,20 +201,39 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &currentRank);
 
     bool statusOfChangeOfSize = false;
-    int matrixSize = SIZE;
-    if (!IsCorrectSize(SIZE, numberOfProcesses)) {
-        matrixSize = FindCorrectSize(SIZE, numberOfProcesses);
+    int matrixSize = ORIGIN_SIZE;
+    if (!IsCorrectSize(ORIGIN_SIZE, numberOfProcesses)) {
+        matrixSize = FindCorrectSize(ORIGIN_SIZE, numberOfProcesses);
         statusOfChangeOfSize = true;
     }
     auto vectorOfSolution = GenerateVectorOfSolution(matrixSize);
-    auto vectorOfRightSide = GenerateVectorOfRightSide(matrixSize, statusOfChangeOfSize, SIZE);
+    auto vectorOfRightSide = GenerateVectorOfRightSide(matrixSize, statusOfChangeOfSize);
     SetFirstApproximation(vectorOfSolution, vectorOfRightSide, matrixSize);
-    vectorOfSolution = MethodOfSimpleIteration(vectorOfSolution, vectorOfRightSide, currentRank, matrixSize, numberOfProcesses, statusOfChangeOfSize, SIZE);
+    double start = MPI_Wtime();
+    vectorOfSolution = MethodOfSimpleIteration(vectorOfSolution, vectorOfRightSide, currentRank, matrixSize, numberOfProcesses, statusOfChangeOfSize);
+    double end = MPI_Wtime();
     if (currentRank == numberOfProcesses - 1) {
-        PrintVector(vectorOfSolution, matrixSize);
+        PrintVector(vectorOfSolution, ORIGIN_SIZE);
+        std::cout << "Time: " << end - start << " (sec)." << std::endl;
     }
     delete[] vectorOfSolution;
     MPI_Finalize();
 
     return 0;
 }
+
+/* 4/3 -> 6/3
+ * origin:
+ * 2 1 1 1
+ * 1 2 1 1
+ * 1 1 2 1
+ * 1 1 1 2
+ *
+ * after:
+ * 2 1 1 1 0 0
+ * 1 2 1 1 0 0
+ * 1 1 2 1 0 0
+ * 1 1 1 2 0 0
+ * 0 0 0 0 0 0
+ * 0 0 0 0 0 0
+ */
