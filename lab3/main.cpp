9ac7a8bb5ab15609a565e0a069f4bc7cb95baea6
Kirill Bolotov <k.y.bolotov@gmail.com>
2023-03-28 23:46:59 +0700

    Added Scatter for B

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -4,10 +4,14 @@
 enum SizeConsts {
     NUMBER_OF_LINES_A = 3,
     NUMBER_OF_COLUMNS_A = 6, /* must be the same with next parameter! */
-    NUMBER_OF_LINES_B = 2, /* must be the same with previous parameter! */
-    NUMBER_OF_COLUMNS_B = 3,
-    HORIZONTAL_NUMBER_OF_COMPUTERS = 2, /* first parameter of grid: p1 */
-    VERTICAL_NUMBER_OF_COMPUTERS = 2 /* second parameter of grid: p2 */
+    NUMBER_OF_LINES_B = 6, /* must be the same with previous parameter! */
+    NUMBER_OF_COLUMNS_B = 4,
+    HORIZONTAL_NUMBER_OF_NODES = 2, /* first parameter of grid: p1 */
+    VERTICAL_NUMBER_OF_NODES = 2 /* second parameter of grid: p2 */
+};
+
+enum DimensionConsts {
+    MAX_DIMENSION = 2
 };
 
 enum Axes {
@@ -15,8 +19,8 @@ enum Axes {
     HORIZONTAL_AXIS = 1
 };
 
-double* GenerateMatrixA() {
-    double* matrixA = new double[NUMBER_OF_LINES_A * NUMBER_OF_COLUMNS_A];
+double *GenerateMatrixA() {
+    double *matrixA = new double[NUMBER_OF_LINES_A * NUMBER_OF_COLUMNS_A];
     int currentPosition = 0;
     for (int i = 0; i < NUMBER_OF_LINES_A; ++i) {
         for (int j = 0; j < NUMBER_OF_COLUMNS_A; ++j) {
@@ -27,8 +31,8 @@ double* GenerateMatrixA() {
     return matrixA;
 }
 
-double* GenerateMatrixB() {
-    double* matrixB = new double[NUMBER_OF_LINES_B * NUMBER_OF_COLUMNS_B];
+double *GenerateMatrixB() {
+    double *matrixB = new double[NUMBER_OF_LINES_B * NUMBER_OF_COLUMNS_B];
     int currentPosition = 0;
     for (int i = 0; i < NUMBER_OF_LINES_B; ++i) {
         for (int j = 0; j < NUMBER_OF_COLUMNS_B; ++j) {
@@ -39,7 +43,7 @@ double* GenerateMatrixB() {
     return matrixB;
 }
 
-void DebugPrint(double* matrix, int lines, int columns) {
+void DebugPrint(double *matrix, int lines, int columns) {
     int currentPosition = 0;
     for (int i = 0; i < lines; ++i) {
         for (int j = 0; j < columns; ++j) {
@@ -51,68 +55,103 @@ void DebugPrint(double* matrix, int lines, int columns) {
     std::cout << "\n";
 }
 
-void SendMatrixAFromRootToVerticalNeighbors(double* matrixA, int matrixSize, int rank, double* receiveBufferForMatrixA,
+void SendMatrixAFromRootToVerticalNeighbors(double *matrixA, int matrixSize, double *receiveBufferForMatrixA,
                                             MPI_Comm COMMUNICATOR, int commSize) {
-//    MPI_Cart_coords(COMMUNICATOR, newRank, 2, coords);
     int sizeOfPartOfMatrixA = matrixSize / commSize;
     int coords[1] = {0};
     int rootRank = 0;
     MPI_Cart_rank(COMMUNICATOR, coords, &rootRank);
-    MPI_Scatter(matrixA, sizeOfPartOfMatrixA, MPI_DOUBLE, receiveBufferForMatrixA, sizeOfPartOfMatrixA, MPI_DOUBLE, rootRank, COMMUNICATOR);
+    MPI_Scatter(matrixA, sizeOfPartOfMatrixA, MPI_DOUBLE, receiveBufferForMatrixA, sizeOfPartOfMatrixA, MPI_DOUBLE,
+                rootRank, COMMUNICATOR);
 }
 
-void SendMatrixBFromRootToHorizontalNeighbors() {
-
+void SendMatrixBFromRootToHorizontalNeighbors(double *matrixB, int numberOfColumnsInPartOfMatrixB,
+                                              double *receiveBufferForMatrixB,
+                                              MPI_Comm COMMUNICATOR, MPI_Datatype VECTOR_TYPE) {
+    int coords[1] = {0};
+    int rootRank = 0;
+    MPI_Cart_rank(COMMUNICATOR, coords, &rootRank);
+    MPI_Scatter(matrixB, numberOfColumnsInPartOfMatrixB, VECTOR_TYPE, receiveBufferForMatrixB,
+                numberOfColumnsInPartOfMatrixB * NUMBER_OF_LINES_B, MPI_DOUBLE, rootRank, COMMUNICATOR);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     int numberOfProcesses;
     int currentRank;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
     MPI_Comm_rank(MPI_COMM_WORLD, &currentRank);
 
-    int dims[2] = {0, 0};
-    int periods[2] = {0, 0};
+    int dims[MAX_DIMENSION] = {0, 0};
+    int periods[MAX_DIMENSION] = {0, 0};
     int reorder = 0;
     MPI_Comm MPI_CUSTOM_2D_GRID;
-    MPI_Dims_create(numberOfProcesses, 2, dims);
-    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &MPI_CUSTOM_2D_GRID);
+    MPI_Dims_create(numberOfProcesses, MAX_DIMENSION, dims);
+    MPI_Cart_create(MPI_COMM_WORLD, MAX_DIMENSION, dims, periods, reorder, &MPI_CUSTOM_2D_GRID);
 
     if (currentRank == 0) {
         std::cout << "DIMS: " << dims[0] << " " << dims[1] << "\n";
     }
 
-    double* matrixA = NULL;
-    double* matrixB = NULL;
-    double* matrixC = NULL; /* result-matrix */
+    double *matrixA = NULL;
+    double *matrixB = NULL;
+    double *matrixC = NULL; /* result-matrix */
 
     MPI_Comm ROOT_HORIZONTAL_NEIGHBORS;
     MPI_Comm ROOT_VERTICAL_NEIGHBORS;
 
-    int horizontal_remain_dims[2] = {0, 1};
-    int vertical_remain_dims[2] = {1, 0};
+    int horizontal_remain_dims[MAX_DIMENSION] = {false, true};
+    int vertical_remain_dims[MAX_DIMENSION] = {true, false};
     MPI_Cart_sub(MPI_CUSTOM_2D_GRID, horizontal_remain_dims, &ROOT_HORIZONTAL_NEIGHBORS);
     MPI_Cart_sub(MPI_CUSTOM_2D_GRID, vertical_remain_dims, &ROOT_VERTICAL_NEIGHBORS);
 
-    int currentRankCoords[2] = {0, 0};
-    MPI_Cart_coords(MPI_CUSTOM_2D_GRID, currentRank, 2, currentRankCoords);
+    int currentRankCoords[MAX_DIMENSION] = {0, 0};
+    MPI_Cart_coords(MPI_CUSTOM_2D_GRID, currentRank, MAX_DIMENSION, currentRankCoords);
     if (currentRankCoords[0] == 0 && currentRankCoords[1] == 0) {
         matrixA = GenerateMatrixA();
         matrixB = GenerateMatrixB();
+        DebugPrint(matrixB, NUMBER_OF_LINES_B, NUMBER_OF_COLUMNS_B);
     }
 
     int sizeOfMatrixA = NUMBER_OF_LINES_A * NUMBER_OF_COLUMNS_A;
-    double* receiveBufferForMatrixA = new double[sizeOfMatrixA / dims[VERTICAL_AXIS]];
+    double *receiveBufferForMatrixA = new double[sizeOfMatrixA / dims[VERTICAL_AXIS]];
     if (currentRank % dims[HORIZONTAL_AXIS] == 0) {
-        SendMatrixAFromRootToVerticalNeighbors(matrixA, sizeOfMatrixA, currentRank, receiveBufferForMatrixA, ROOT_VERTICAL_NEIGHBORS, dims[VERTICAL_AXIS]);
+        SendMatrixAFromRootToVerticalNeighbors(matrixA, sizeOfMatrixA, receiveBufferForMatrixA, ROOT_VERTICAL_NEIGHBORS,
+                                               dims[VERTICAL_AXIS]);
     }
 
-    std::cout << "CURRENT RANK: " << currentRank << "\n";
+    std::cout << "(Scatter A) CURRENT RANK: " << currentRank << "\n";
     DebugPrint(receiveBufferForMatrixA, NUMBER_OF_LINES_A / dims[VERTICAL_AXIS], NUMBER_OF_COLUMNS_A);
 
-    double* receiveBufferForMatrixB;
-    // TODO: create MPI_Type for matrix B before Scatter.
+    MPI_Datatype COLUMN_TYPE;
+    /* 1 - distance between elements in column */
+    MPI_Type_vector(NUMBER_OF_LINES_B, 1, NUMBER_OF_COLUMNS_B, MPI_DOUBLE, &COLUMN_TYPE);
+    MPI_Type_commit(&COLUMN_TYPE);
+
+    MPI_Datatype RESIZED_COLUMN_TYPE;
+    MPI_Type_create_resized(COLUMN_TYPE, 0, (NUMBER_OF_COLUMNS_B / dims[VERTICAL_AXIS]) * sizeof(double),
+                            &RESIZED_COLUMN_TYPE);
+    MPI_Type_commit(&RESIZED_COLUMN_TYPE);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    int sizeOfMatrixB = NUMBER_OF_LINES_B * NUMBER_OF_COLUMNS_B;
+    double *receiveBufferForMatrixB = new double[sizeOfMatrixB / dims[HORIZONTAL_AXIS]];
+    if (currentRank < dims[HORIZONTAL_AXIS]) {
+        SendMatrixBFromRootToHorizontalNeighbors(matrixB, NUMBER_OF_COLUMNS_B / dims[HORIZONTAL_AXIS],
+                                                 receiveBufferForMatrixB, ROOT_HORIZONTAL_NEIGHBORS,
+                                                 RESIZED_COLUMN_TYPE);
+    }
+
+    if (currentRank == 0) {
+        std::cout << "\n num * lines " << NUMBER_OF_COLUMNS_B / dims[HORIZONTAL_AXIS] * NUMBER_OF_LINES_B << " num: "
+                  << NUMBER_OF_COLUMNS_B / dims[HORIZONTAL_AXIS] << "\n";
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    std::cout << "(Scatter B) CURRENT RANK: " << currentRank << "\n";
+    MPI_Barrier(MPI_COMM_WORLD);
+    DebugPrint(receiveBufferForMatrixB, NUMBER_OF_LINES_B / dims[HORIZONTAL_AXIS], NUMBER_OF_COLUMNS_B);
+    MPI_Barrier(MPI_COMM_WORLD);
 
 //    DebugPrint(matrixA, NUMBER_OF_LINES_A, NUMBER_OF_COLUMNS_A);
 //    DebugPrint(matrixB, NUMBER_OF_LINES_B, NUMBER_OF_COLUMNS_B);
@@ -130,4 +169,26 @@ int main(int argc, char** argv) {
     }
     MPI_Barrier(MPI_COMM_WORLD);
     std::cout << currentRank << "::::: " << coords[0] << " " << coords[1] << "\n";
+ */
+
+/*
+ * COLUMN_TYPE_TEST
+    if (currentRank == 0) {
+        double matrix[6][3];
+        double count = 10;
+        for (int i = 0; i < 6; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                matrix[i][j] = count;
+                ++count;
+            }
+        }
+        MPI_Send(&matrix[0][0], 1, COLUMN_TYPE, 1, 0, MPI_COMM_WORLD);
+    } else if (currentRank == 1) {
+        double recvBuf[6];
+        MPI_Recv(recvBuf, 6, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        std::cout << "TEST:\n";
+        for (int i = 0; i < 6; ++i) {
+            std::cout << recvBuf[i] << " ";
+        }
+    }
  */
