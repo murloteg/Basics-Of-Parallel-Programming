e1163ca8460f558e4ab8edbdbd52e7c7f036778d
Kirill Bolotov <k.y.bolotov@gmail.com>
2023-04-15 18:53:50 +0700

    Finished not-parallel version

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <cfloat>
 
+#define CHECK_PARAMETER 0.01
 #define EPSILON 0.00000001
 enum GeneralParameters {
     AREA_START_COORDINATE = -1,
     AREA_END_COORDINATE = 1,
     EQUATION_PARAMETER = 100000,
     INITIAL_APPROXIMATION = 0,
-    BOUNDARY_CONDITIONALS = 2
 };
 
 enum GridParameters {
-    NUMBER_OF_POINTS_X = 5,
-    NUMBER_OF_POINTS_Y = 5,
-    NUMBER_OF_POINTS_Z = 3,
+    NUMBER_OF_POINTS_X = 7,
+    NUMBER_OF_POINTS_Y = 4,
+    NUMBER_OF_POINTS_Z = 3
 };
 
 /* f(x, y, z) = x^2 + y^2 + z^2 */
@@ -35,24 +35,15 @@ double CalculateDistanceBetweenPoints(int numberOfPoints) {
 }
 
 bool isBoundaryPositionInXYPlane(int x, int y, int numberOfPointsX, int numberOfPointsY) {
-    if (x == 0 || (x == numberOfPointsX - 1) || y == 0 || (y == numberOfPointsY - 1)) {
-        return true;
-    }
-    return false;
+    return (x == 0 || (x == numberOfPointsX - 1) || y == 0 || (y == numberOfPointsY - 1));
 }
 
 bool isBoundaryPositionInXZPlane(int x, int z, int numberOfPointsX, int numberOfPointsZ) {
-    if (x == 0 || (x == numberOfPointsX - 1) || z == 0 || (z == numberOfPointsZ - 1)) {
-        return true;
-    }
-    return false;
+    return (x == 0 || (x == numberOfPointsX - 1) || z == 0 || (z == numberOfPointsZ - 1));
 }
 
 bool isBoundaryPositionInYZPlane(int y, int z, int numberOfPointsY, int numberOfPointsZ) {
-    if (y == 0 || (y == numberOfPointsY - 1) || z == 0 || (z == numberOfPointsZ - 1)) {
-        return true;
-    }
-    return false;
+    return (y == 0 || (y == numberOfPointsY - 1) || z == 0 || (z == numberOfPointsZ - 1));
 }
 
 bool isBoundaryPosition(int x, int y, int z, int numberOfPointsX, int numberOfPointsY, int numberOfPointsZ) {
@@ -74,6 +65,10 @@ void SetInitialApproximationToGrid(double* gridOfPoints, int numberOfPointsX, in
     }
 }
 
+double GetCurrentCoordinateValue(int startPointCoordinate, int index, double distanceBetweenPoints) {
+    return startPointCoordinate + index * distanceBetweenPoints;
+}
+
 /* position formula: (i * N_y * N_x + j * N_x + k) */
 void SetBoundaryConditionals(double* gridOfPoints, int numberOfPointsX, int numberOfPointsY, int numberOfPointsZ) {
     int position = 0;
@@ -82,7 +77,14 @@ void SetBoundaryConditionals(double* gridOfPoints, int numberOfPointsX, int numb
             for (int k = 0; k < numberOfPointsX; ++k) {
                 position = i * numberOfPointsX * numberOfPointsY + j * numberOfPointsX + k;
                 if (isBoundaryPosition(k, j, i, numberOfPointsX, numberOfPointsY, numberOfPointsZ)) {
-                    gridOfPoints[position] = BOUNDARY_CONDITIONALS;
+                    double distanceBetweenCoordinateX = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_X);
+                    double distanceBetweenCoordinateY = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_Y);
+                    double distanceBetweenCoordinateZ = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_Z);
+
+                    double localX = GetCurrentCoordinateValue(AREA_START_COORDINATE, k, distanceBetweenCoordinateX);
+                    double localY = GetCurrentCoordinateValue(AREA_START_COORDINATE, j, distanceBetweenCoordinateY);
+                    double localZ = GetCurrentCoordinateValue(AREA_START_COORDINATE, i, distanceBetweenCoordinateZ);
+                    gridOfPoints[position] = CalculateFunctionValue(localX, localY, localZ);
                 }
             }
         }
@@ -105,24 +107,20 @@ double FindLeftMultiplier(double distanceBetweenCoordinateX, double distanceBetw
                 + 2 / (distanceBetweenCoordinateZ * distanceBetweenCoordinateZ) + EQUATION_PARAMETER);
 }
 
-double GetCurrentCoordinateValue(int startPointCoordinate, int index, double distanceBetweenPoints) {
-    return startPointCoordinate + index * distanceBetweenPoints;
-}
-
-double FindRightMultiplier(int indexI, int indexJ, int indexK,double distanceBetweenCoordinateX,
+double FindRightMultiplier(double* gridOfPoints, int indexI, int indexJ, int indexK, double distanceBetweenCoordinateX,
                            double distanceBetweenCoordinateY, double distanceBetweenCoordinateZ) {
     double localX = GetCurrentCoordinateValue(AREA_START_COORDINATE, indexK, distanceBetweenCoordinateX);
     double localY = GetCurrentCoordinateValue(AREA_START_COORDINATE, indexJ, distanceBetweenCoordinateY);
     double localZ = GetCurrentCoordinateValue(AREA_START_COORDINATE, indexI, distanceBetweenCoordinateZ);
 
-    double firstNeighborZ = CalculateFunctionValue(localX, localY, GetCurrentCoordinateValue(AREA_START_COORDINATE, indexI + 1, distanceBetweenCoordinateZ));
-    double secondNeighborZ = CalculateFunctionValue(localX, localY, GetCurrentCoordinateValue(AREA_START_COORDINATE, indexI - 1, distanceBetweenCoordinateZ));
+    double firstNeighborZ = gridOfPoints[(indexI + 1) * NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y + indexJ * NUMBER_OF_POINTS_X + indexK];
+    double secondNeighborZ = gridOfPoints[(indexI - 1) * NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y + indexJ * NUMBER_OF_POINTS_X + indexK];
 
-    double firstNeighborY = CalculateFunctionValue(localX, GetCurrentCoordinateValue(AREA_START_COORDINATE, indexJ + 1, distanceBetweenCoordinateY), localZ);
-    double secondNeighborY = CalculateFunctionValue(localX, GetCurrentCoordinateValue(AREA_START_COORDINATE, indexJ - 1, distanceBetweenCoordinateY), localZ);
+    double firstNeighborY = gridOfPoints[indexI * NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y + (indexJ + 1) * NUMBER_OF_POINTS_X + indexK];
+    double secondNeighborY = gridOfPoints[indexI * NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y + (indexJ - 1) * NUMBER_OF_POINTS_X + indexK];
 
-    double firstNeighborX = CalculateFunctionValue(GetCurrentCoordinateValue(AREA_START_COORDINATE, indexK + 1, distanceBetweenCoordinateX), localY, localZ);
-    double secondNeighborX = CalculateFunctionValue(GetCurrentCoordinateValue(AREA_START_COORDINATE, indexK - 1, distanceBetweenCoordinateX), localY, localZ);
+    double firstNeighborX = gridOfPoints[indexI * NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y + indexJ * NUMBER_OF_POINTS_X + indexK + 1];
+    double secondNeighborX = gridOfPoints[indexI * NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y + indexJ * NUMBER_OF_POINTS_X + indexK - 1];
 
     double rightPartValue = CalculateRightPartOfEquationValue(localX, localY, localZ);
     return (firstNeighborZ + secondNeighborZ) / distanceBetweenCoordinateZ * distanceBetweenCoordinateZ
@@ -131,17 +129,57 @@ double FindRightMultiplier(int indexI, int indexJ, int indexK,double distanceBet
             - rightPartValue;
 }
 
-void UpdateMaxDifference(double& maxDifference, double* gridOfPoints, double* nextStateOfGrid) {
+double GetCheckValue(int indexI, int indexJ, int indexK, double distanceBetweenCoordinateX,
+                     double distanceBetweenCoordinateY, double distanceBetweenCoordinateZ) {
+    double localX = GetCurrentCoordinateValue(AREA_START_COORDINATE, indexK, distanceBetweenCoordinateX);
+    double localY = GetCurrentCoordinateValue(AREA_START_COORDINATE, indexJ, distanceBetweenCoordinateY);
+    double localZ = GetCurrentCoordinateValue(AREA_START_COORDINATE, indexI, distanceBetweenCoordinateZ);
+    return CalculateFunctionValue(localX, localY, localZ);
+}
+
+bool CompareResult(double* gridOfPoints) {
+    double distanceBetweenCoordinateX = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_X);
+    double distanceBetweenCoordinateY = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_Y);
+    double distanceBetweenCoordinateZ = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_Z);
+
+    double maxDifference = DBL_MIN;
     for (int i = 0; i < NUMBER_OF_POINTS_Z; ++i) {
         for (int j = 0; j < NUMBER_OF_POINTS_Y; ++j) {
             for (int k = 0; k < NUMBER_OF_POINTS_X; ++k) {
                 int position = i * NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y + j * NUMBER_OF_POINTS_X + k;
-                if (!isBoundaryPosition(k, j, i, NUMBER_OF_POINTS_X, NUMBER_OF_POINTS_Y, NUMBER_OF_POINTS_Z)) {
-                    double currentDiff = CalculateDifferenceBetweenPreviousValueAndNewValue(gridOfPoints[position], nextStateOfGrid[position]);
-                    maxDifference = std::min(maxDifference, currentDiff);
+                double checkValue = GetCheckValue(i, j, k, distanceBetweenCoordinateX, distanceBetweenCoordinateY, distanceBetweenCoordinateZ);
+                maxDifference = std::max(maxDifference, std::abs(gridOfPoints[position] - checkValue));
+            }
+        }
+    }
+    return (maxDifference > DBL_MIN && maxDifference < CHECK_PARAMETER);
+}
+
+void IterativeProcessOfJacobiAlgorithm(double* gridOfPoints) {
+    double distanceBetweenCoordinateX = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_X);
+    double distanceBetweenCoordinateY = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_Y);
+    double distanceBetweenCoordinateZ = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_Z);
+
+    double maxDifference = DBL_MAX;
+    while (maxDifference >= EPSILON) {
+        for (int i = 0; i < NUMBER_OF_POINTS_Z; ++i) {
+            for (int j = 0; j < NUMBER_OF_POINTS_Y; ++j) {
+                for (int k = 0; k < NUMBER_OF_POINTS_X; ++k) {
+                    int position = i * NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y + j * NUMBER_OF_POINTS_X + k;
+                    if (!isBoundaryPosition(k, j, i, NUMBER_OF_POINTS_X, NUMBER_OF_POINTS_Y, NUMBER_OF_POINTS_Z)) {
+                        double previousValue = gridOfPoints[position];
+                        gridOfPoints[position] = FindLeftMultiplier(distanceBetweenCoordinateX, distanceBetweenCoordinateY, distanceBetweenCoordinateZ)
+                                * FindRightMultiplier(gridOfPoints, i, j, k, distanceBetweenCoordinateX, distanceBetweenCoordinateY, distanceBetweenCoordinateZ);
+                        maxDifference = std::min(maxDifference, CalculateDifferenceBetweenPreviousValueAndNewValue(previousValue, gridOfPoints[position]));
+                    }
                 }
             }
         }
+    }
+    if (CompareResult(gridOfPoints)) {
+        std::cout << "CORRECT\n";
+    } else {
+        std::cout << "INCORRECT!\n";
     }
 }
 
@@ -160,54 +198,13 @@ void DebugPrintOfGrid(double* gridOfPoints, int numberOfPointsX, int numberOfPoi
     std::cout << "\n";
 }
 
-void CopyFromNextStateOfGridToPreviousStateOfGrid(double* gridOfPoints, double* nextStateOfGrid, int numberOfPointsX,
-                                                  int numberOfPointsY, int numberOfPointsZ) {
-    for (int i = 0; i < NUMBER_OF_POINTS_Z; ++i) {
-        for (int j = 0; j < NUMBER_OF_POINTS_Y; ++j) {
-            for (int k = 0; k < NUMBER_OF_POINTS_X; ++k) {
-                int position = i * NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y + j * NUMBER_OF_POINTS_X + k;
-                gridOfPoints[position] = nextStateOfGrid[position];
-            }
-        }
-    }
-}
-
-void IterativeProcessOfJacobiAlgorithm(double* gridOfPoints) {
-    double distanceBetweenCoordinateX = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_X);
-    double distanceBetweenCoordinateY = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_Y);
-    double distanceBetweenCoordinateZ = CalculateDistanceBetweenPoints(NUMBER_OF_POINTS_Z);
-
-    double* nextStateOfGrid = new double[NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y * NUMBER_OF_POINTS_Z];
-    double maxDifference = DBL_MAX;
-    while (maxDifference >= EPSILON) {
-        for (int i = 0; i < NUMBER_OF_POINTS_Z; ++i) {
-            for (int j = 0; j < NUMBER_OF_POINTS_Y; ++j) {
-                for (int k = 0; k < NUMBER_OF_POINTS_X; ++k) {
-                    int position = i * NUMBER_OF_POINTS_X * NUMBER_OF_POINTS_Y + j * NUMBER_OF_POINTS_X + k;
-                    nextStateOfGrid[position] = gridOfPoints[position];
-                    if (!isBoundaryPosition(k, j, i, NUMBER_OF_POINTS_X, NUMBER_OF_POINTS_Y, NUMBER_OF_POINTS_Z)) {
-                        nextStateOfGrid[position] = FindLeftMultiplier(distanceBetweenCoordinateX, distanceBetweenCoordinateY, distanceBetweenCoordinateZ)
-                                * FindRightMultiplier(i, j, k, distanceBetweenCoordinateX, distanceBetweenCoordinateY, distanceBetweenCoordinateZ);
-                    }
-                }
-            }
-        }
-        UpdateMaxDifference(maxDifference, gridOfPoints, nextStateOfGrid);
-        CopyFromNextStateOfGridToPreviousStateOfGrid(gridOfPoints, nextStateOfGrid, NUMBER_OF_POINTS_X, NUMBER_OF_POINTS_Y, NUMBER_OF_POINTS_Z);
-        std::cout << "max diff: " << maxDifference << "\n";
-    }
-    delete[] nextStateOfGrid;
-}
-
 void CleanUp(double* gridOfPoints) {
     delete[] gridOfPoints;
 }
 
 int main() {
     double* gridOfPoints = CreateGridOfPoints(NUMBER_OF_POINTS_X, NUMBER_OF_POINTS_Y, NUMBER_OF_POINTS_Z);
-//    DebugPrintOfGrid(gridOfPoints, NUMBER_OF_POINTS_X, NUMBER_OF_POINTS_Y, NUMBER_OF_POINTS_Z);
     IterativeProcessOfJacobiAlgorithm(gridOfPoints);
-    DebugPrintOfGrid(gridOfPoints, NUMBER_OF_POINTS_X, NUMBER_OF_POINTS_Y, NUMBER_OF_POINTS_Z);
     CleanUp(gridOfPoints);
 
     return 0;
